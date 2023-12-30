971a65e6e5d3774cb19c7f459a33386c70ecea1a
Stephen Parker <sparker888@gmail.com>
2023-12-30 15:27:55 -0500

    edit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -239,7 +239,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-slate-800 dark:text-slate-100 sm:text-5xl">
-            Solopreneur, digital brand manager, and photographer.
+            Solopreneur, web developer, and photographer.
           </h1>
           <p className="mt-6 text-xl text-slate-600 dark:text-slate-400">
             🤙🏻 Aloha, I&apos;m Stephen Parker aka @sparker888. I enjoy 🧑🏼‍🚀
