9191040c1ae53b208bef1425a7a29d6451619b0b
Stephen Parker <sparker888@gmail.com>
2023-12-31 16:18:08 -0500

    ✨ Final polish

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -243,8 +243,8 @@ export default async function Home() {
           </h1>
           <p className="mt-6 text-xl text-slate-600 dark:text-slate-400">
             🤙🏻 Aloha, I go by @sparker888. I specialize in 💻 web development
-            and digital design. I'm a PAA 📸 photographer, FAA-licensed 🚁 drone
-            pilot, and a Midjourney 🧭 AI expert. 🚀 Let's fly!
+            and digital design. I'm a PPA 📸 photographer, FAA-licensed 🚁 drone
+            pilot, and Midjourney 🧭 AI expert. 🚀 Let's fly! ✨
           </p>
           <div className="mt-6 flex gap-6 text-sky-600 dark:text-amber-500">
             <SocialLink
