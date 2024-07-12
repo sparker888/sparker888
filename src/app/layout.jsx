e99991551539b4fe4eb1cb0600197932abccb413
Stephen Parker <sparker888@gmail.com>
2024-07-12 13:44:08 -0400

    add mid-july updates

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,10 +8,10 @@ export const metadata = {
   title: {
     template: '%s - @sparker888',
     default:
-      'Steve Parker 💻 Web developer & 📸 PPA photographer sparker888@gmail.com.',
+      'Steve Parker @sparker888.',
   },
   description:
-    'I&apos;m Steve Parker aka @sparker888. I help companies build their online brands through 💻 web development and digital design. I&apos;m a PPA 📸 photographer, FAA-licensed 🚁 drone pilot and a Midjourney 🧭 AI expert.',
+    'I&apos;m Steve Parker aka @sparker888. I help companies build their online brands through web development and digital design. I&apos;m a PPA 📸 photographer, FAA-licensed 🚁 drone pilot and a Midjourney 🧭 AI expert.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <PlausibleProvider domain="sparker888.com">
       <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-        <body className="bg-pattern dark:bg-pattern flex h-full">
+        <body className="flex h-full bg-pattern dark:bg-pattern">
           <Providers>
             <div className="flex w-full">
               <Layout>{children}</Layout>
