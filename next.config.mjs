182972cc4bb10a90516b98d8f92200255477b8a6
Stephen Parker <sparker888@gmail.com>
2024-07-22 13:03:38 -0400

    updating gifs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,44 @@
-import { FontaineTransform } from 'fontaine';
-import rehypePrism from '@mapbox/rehype-prism';
-import nextMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
+import { FontaineTransform } from 'fontaine'
+import rehypePrism from '@mapbox/rehype-prism'
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   webpack: (config, { isServer }) => {
     // FontaineTransform setup
-    config.plugins = config.plugins || [];
+    config.plugins = config.plugins || []
     config.plugins.push(
       FontaineTransform.webpack({
         fallbacks: ['Arial'],
         resolvePath: (id) => `./public${id}`,
       }),
-    );
+    )
 
     // Add alias for react-dom/server.edge
     config.resolve.alias = {
       ...config.resolve.alias,
       'react-dom/server.edge': 'react-dom/server',
-      'react-dom$': 'react-dom/profiling', // Add this line
-    };
+      'react-dom$': 'react-dom/profiling',
+    }
 
-    return config;
+    return config
   },
-};
+  // Update image configuration
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
@@ -33,6 +46,6 @@ const withMDX = nextMDX({
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
   },
-});
+})
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
