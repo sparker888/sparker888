48826f74af5fec0705be38d319b1b7253c7c3b26
Stephen Parker <sparker888@gmail.com>
2024-08-07 11:24:35 -0400

    added seo components to layout.jsx and images

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,13 +5,33 @@ import PlausibleProvider from 'next-plausible'
 import '@/styles/tailwind.css'
 
 export const metadata = {
+  metadataBase: new URL('https://sparker888.com'),
   title: {
     template: '%s - @sparker888',
-    default:
-      'Steve Parker @sparker888.',
+    default: 'Stephen Parker @sparker888',
   },
-  description:
-    'I&apos;m Steve Parker aka @sparker888. I help companies build their online brands through web development and digital design. I&apos;m a PPA 📸 photographer, FAA-licensed 🚁 drone pilot and a Midjourney 🧭 AI expert.',
+  description: 'I\'m Stephen Parker aka @sparker888. I help companies build their online brands. I produce Light. Speed.✨ websites & apps as the Founder of Gravital Digital. My passion is producing sites that deliver incredible value and delight my clients. Here\'s an example case study. I also support 🧭 Midjourney AI image creators with an app called Midjourney Prompt Manager. Let\'s fly! 🚀',
+  openGraph: {
+    title: 'Stephen Parker @sparker888',
+    description: 'Light. Speed.✨ websites & apps | Founder of Gravital Digital | Producer of Midjourney Prompt Manager',
+    url: 'https://sparker888.com',
+    siteName: '@sparker888',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Stephen Parker @sparker888',
+    description: 'Light. Speed.✨ websites & apps | Founder of Gravital Digital | Producer of Midjourney Prompt Manager',
+    images: ['/twitter-image.png'],
+  },
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
